fc71830c951c7b3104d2175cc1994561af99ac0b
Maksim <makc.pinsk@gmail.com>
2020-09-20 21:11:46 +0300

    change bag

--- decision/bin/task1.py
+++ decision/bin/task1.py
@@ -1,37 +1,13 @@
-# input_data = input().strip().split(' ')
-# table_size = int(input_data[0])
-# digit = int(input_data[1])
-# numbers_digit = 0
-# if 1 <= table_size <= 10 ** 5 and 1 <= digit <= 10 ** 9:
-#     for i in range(1, table_size + 1):
-#         if digit % i == 0 and digit / i <= table_size:
-#             numbers_digit += 1
-#     print(numbers_digit)
-
-
 def table_pifagor(input_data):
-    input_data = input().strip().split(' ')
-    table_size = int(input_data[0])
-    digit = int(input_data[1])
+    data = input_data.strip().split(' ')
+    table_size = int(data[0])
+    digit = int(data[1])
     numbers_digit = 0
     if 1 <= table_size <= 10 ** 5 and 1 <= digit <= 10 ** 9:
         for i in range(1, table_size + 1):
             if digit % i == 0 and digit / i <= table_size:
                 numbers_digit += 1
-        print(numbers_digit)
     return numbers_digit
 
-import timeit
-code_to_test ='''
-input_data = input().strip().split(' ')
-table_size = int(input_data[0])
-digit = int(input_data[1])
-numbers_digit = 0
-if 1 <= table_size <= 10 ** 5 and 1 <= digit <= 10 ** 9:
-    for i in range(1, table_size + 1):
-        if digit % i == 0 and digit / i <= table_size:
-            numbers_digit += 1
-    print(numbers_digit)
-'''
-elapsed_time = timeit.timeit(code_to_test, number=1)
-print(elapsed_time)
+
+print(table_pifagor(input()))
---
+++ decision/task2.py
@@ -0,0 +1,5 @@
+from random import randint
+mass = [randint(1,10**5) for i in range(9)]
+print(mass)
+mass_sorted = sorted(mass)
+print(mass_sorted)
