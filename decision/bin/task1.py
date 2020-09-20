b5ab4b27e7943cf469611fcb45d685ec8a52d397
Maksim <makc.pinsk@gmail.com>
2020-09-20 14:06:11 +0300

    test

--- decision/bin/task1.py
+++ decision/bin/task1.py
@@ -1,3 +1,28 @@
+# input_data = input().strip().split(' ')
+# table_size = int(input_data[0])
+# digit = int(input_data[1])
+# numbers_digit = 0
+# if 1 <= table_size <= 10 ** 5 and 1 <= digit <= 10 ** 9:
+#     for i in range(1, table_size + 1):
+#         if digit % i == 0 and digit / i <= table_size:
+#             numbers_digit += 1
+#     print(numbers_digit)
+
+
+def table_pifagor(input_data):
+    input_data = input().strip().split(' ')
+    table_size = int(input_data[0])
+    digit = int(input_data[1])
+    numbers_digit = 0
+    if 1 <= table_size <= 10 ** 5 and 1 <= digit <= 10 ** 9:
+        for i in range(1, table_size + 1):
+            if digit % i == 0 and digit / i <= table_size:
+                numbers_digit += 1
+        print(numbers_digit)
+    return numbers_digit
+
+import timeit
+code_to_test ='''
 input_data = input().strip().split(' ')
 table_size = int(input_data[0])
 digit = int(input_data[1])
@@ -7,3 +32,6 @@ if 1 <= table_size <= 10 ** 5 and 1 <= digit <= 10 ** 9:
         if digit % i == 0 and digit / i <= table_size:
             numbers_digit += 1
     print(numbers_digit)
+'''
+elapsed_time = timeit.timeit(code_to_test, number=1)
+print(elapsed_time)
---
+++ decision/bin/task1test.py
@@ -0,0 +1,13 @@
+import unittest
+from .task1 import table_pifagor
+
+class PifagorTesy(unittest.TestCase):
+
+    def test_pifagor(self):
+        self.assertEqual(table_pifagor('13 36'), 5)
+        self.assertEqual(table_pifagor('15 225'), 1)
+        self.assertEqual(table_pifagor('14 140'), 2)
+        self.assertEqual(table_pifagor(' 36'), 5)
+        self.assertEqual(table_pifagor('15 30'), 6)
+        self.assertEqual(table_pifagor('15 120'), 4)
+        self.assertEqual(table_pifagor('6 12'), 4)
